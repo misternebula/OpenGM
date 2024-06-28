1ac0694b66afe57cc9ec1b0a3a2fe3fa7085512c
_nebula <41904486+misternebula@users.noreply.github.com>
2024-06-28 16:35:41 +0100

    Create TextureGroup.cs

---
+++ SerializedFiles/TextureGroup.cs
@@ -0,0 +1,10 @@
+﻿namespace DELTARUNITYStandalone.SerializedFiles;
+
+[Serializable]
+public class TextureGroup
+{
+	public string GroupName;
+	public string[] TexturePages;
+	public int[] Sprites;
+	public int[] Fonts;
+}
