26cb7d42112ec68b2b5e3c5f46ec8941a3e6b019
_nebula <41904486+misternebula@users.noreply.github.com>
2024-07-14 01:18:39 +0100

    add other GMConstants

--- OpenGM/VirtualMachine/GMConstants.cs
+++ OpenGM/VirtualMachine/GMConstants.cs
@@ -9,7 +9,11 @@ public static class GMConstants
 	public const int all = -3;
 	public const int noone = -4;
 	public const int global = -5;
+	public const int builtin = -6;
 	public const int local = -7;
+	// fuck you -9 doesnt exist
 	public const int stacktop = -9;
+	// fuck you -10 to -14 doesnt exist either!!!
 	public const int argument = -15;
+	public const int @static = -16;
 }
