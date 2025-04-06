ca75e0402fcd943ea47c79d0eb43a3efefcdb63d
JohnCorby <williamforgames6@gmail.com>
2025-04-05 23:02:29 -0700

    wtf

--- OpenGM.Tests/SoundTests.cs
+++ OpenGM.Tests/SoundTests.cs
@@ -31,9 +31,9 @@ public class SoundTests
         {
             using var reader = new VorbisReader("AUDIO_INTRONOISE.ogg");
             var total = 0;
+            var buffer = new float[reader.SampleRate];
             while (true)
             {
-                var buffer = new float[1024];
                 var count = reader.ReadSamples(buffer, 0, buffer.Length);
                 DebugLog.Log($"read {count} total {total} of {reader.TotalSamples * reader.Channels}");
                 total += count;
@@ -50,14 +50,14 @@ public class SoundTests
             using var vorbis = Vorbis.FromMemory(File.ReadAllBytes("AUDIO_INTRONOISE.ogg"));
             var total = 0;
             vorbis.SubmitBuffer();
-            var samples = new float[1024];
+            var samples = new float[vorbis.SampleRate];
             while (true)
             {
                 int count;
                 fixed (float* ptr = samples)
                     count = StbVorbis.stb_vorbis_get_samples_float_interleaved(vorbis.StbVorbis, vorbis.Channels, ptr, samples.Length);
                 total += count;
-                DebugLog.LogInfo($"read {count} total {total} of {vorbis.StbVorbis.total_samples*vorbis.Channels}");
+                DebugLog.LogInfo($"read {count} total {total} of {vorbis.StbVorbis.total_samples * vorbis.Channels}");
                 if (count == 0) break;
             }
 
