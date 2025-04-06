bb0b7b012d0c077dbdf81a1f0fc1d4083a51ed39
JohnCorby <williamforgames6@gmail.com>
2025-04-05 21:55:12 -0700

    add ogg tests

---
+++ OpenGM.Tests/SoundTests.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NVorbis;
+using OpenGM.VirtualMachine;
+
+namespace OpenGM.Tests;
+
+[TestClass]
+public class SoundTests
+{
+    [TestMethod]
+    public void TestGoodOgg()
+    {
+        using var vorbisReader = new VorbisReader("AUDIO_INTRONOISE.ogg");
+        var samples = new float[vorbisReader.TotalSamples * vorbisReader.Channels];
+        vorbisReader.ReadSamples(samples, 0, samples.Length);
+    }
+
+    [TestMethod]
+    public void TestBadOgg()
+    {
+        // this hangs rn
+        using var vorbisReader = new VorbisReader("mus_menu1.ogg");
+        var samples = new float[vorbisReader.TotalSamples * vorbisReader.Channels];
+        // vorbisReader.ReadSamples(samples, 0, samples.Length);
+    }
+}
