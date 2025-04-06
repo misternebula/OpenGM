53bd624e0c835372d2e9592e8a92041b3257c01b
JohnCorby <williamforgames6@gmail.com>
2025-04-05 22:58:41 -0700

    compare vorbis and stb. its being weird rn

--- OpenGM.Tests/SoundTests.cs
+++ OpenGM.Tests/SoundTests.cs
@@ -1,7 +1,7 @@
-﻿using System.Collections;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using NVorbis;
-using OpenGM.VirtualMachine;
+using OpenGM.IO;
+using StbVorbisSharp;
 
 namespace OpenGM.Tests;
 
@@ -23,5 +23,44 @@ public class SoundTests
         using var vorbisReader = new VorbisReader("mus_menu1.ogg");
         var samples = new float[vorbisReader.TotalSamples * vorbisReader.Channels];
         // vorbisReader.ReadSamples(samples, 0, samples.Length);
+    }
+
+    [TestMethod]
+    public unsafe void VorbisVsStb()
+    {
+        {
+            using var reader = new VorbisReader("AUDIO_INTRONOISE.ogg");
+            var total = 0;
+            while (true)
+            {
+                var buffer = new float[1024];
+                var count = reader.ReadSamples(buffer, 0, buffer.Length);
+                DebugLog.Log($"read {count} total {total} of {reader.TotalSamples * reader.Channels}");
+                total += count;
+                if (count == 0) break;
+            }
+        }
+    
+        DebugLog.LogInfo("-----------------------------------------");
+        DebugLog.LogInfo("-----------------------------------------");
+        DebugLog.LogInfo("-----------------------------------------");
+
+        {
+            // https://nothings.org/stb_vorbis/samples/sample.c
+            using var vorbis = Vorbis.FromMemory(File.ReadAllBytes("AUDIO_INTRONOISE.ogg"));
+            var total = 0;
+            vorbis.SubmitBuffer();
+            var samples = new float[1024];
+            while (true)
+            {
+                int count;
+                fixed (float* ptr = samples)
+                    count = StbVorbis.stb_vorbis_get_samples_float_interleaved(vorbis.StbVorbis, vorbis.Channels, ptr, samples.Length);
+                total += count;
+                DebugLog.LogInfo($"read {count} total {total} of {vorbis.StbVorbis.total_samples*vorbis.Channels}");
+                if (count == 0) break;
+            }
+
+        }
     }
 }
