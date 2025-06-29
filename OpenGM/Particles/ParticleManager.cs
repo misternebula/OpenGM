0b2825607fdb89b0e6c16695dae39f17e66107fd
_nebula <41904486+misternebula@users.noreply.github.com>
2025-06-29 22:29:11 +0100

    little bit more particles

--- OpenGM/Particles/ParticleManager.cs
+++ OpenGM/Particles/ParticleManager.cs
@@ -47,7 +47,7 @@
 			{
 				if (emitter.Created && emitter.Number != 0)
 				{
-					// burst
+					EmitterBurst(sys, emitter, emitter.PartType, emitter.Number);
 				}
 			}
 		}
@@ -65,6 +65,80 @@
 		public static void HandleShape(ParticleSystem sys)
 		{
 
+		}
+
+		public static void EmitterBurst(ParticleSystem system, ParticleEmitter emitter, int parttype, int number)
+		{
+
+		}
+
+		public static Particle CreateParticle(double x, double y, int particleTypeId)
+		{
+			var p = new Particle();
+			var type = PartTypes[particleTypeId];
+
+			p.Alive = true;
+			p.PartType = particleTypeId;
+			p.X = x;
+			p.Y = y;
+			p.XOld = x;
+			p.YOld = y;
+			p.Speed = MyRandom(type.SpeedMin, type.SpeedMax, 0);
+			p.Dir = MyRandom(type.DirMin, type.DirMax, 0);
+			p.Angle = MyRandom(type.AngleMin, type.AngleMax, 0);
+			p.Lifetime = (int)MyRandom(type.LifeMin, type.LifeMax, 0); // TODO : exactly how is this rounded?
+			p.Age = 0;
+			// compute color
+			p.Alpha = type.AlphaStart;
+			p.Size = MyRandom(type.SizeMin, type.SizeMax, 0);
+			if (!type.SpriteRandom)
+			{
+				p.SpriteStart = 0;
+			}
+			else
+			{
+				p.SpriteStart = (int)GMRandom.YYRandom(10000);
+			}
+			p.Ran = (int)GMRandom.YYRandom(10000);
+		}
+
+		private static double MyRandom(double min, double max, double dist)
+		{
+			var range = max - min;
+
+			if (range <= 0)
+			{
+				return min;
+			}
+
+			double xx;
+
+			switch (dist)
+			{
+				case 0: // PART_EDISTR_LINEAR
+					return (GMRandom.fYYRandom() * range) + min;
+				case 1: // PART_EDISTR_GAUSSIAN
+					do
+					{
+						xx = (GMRandom.fYYRandom() - 0.5) * 6;
+					} while (Math.Exp(-(xx * xx) * 0.5) <= GMRandom.fYYRandom());
+
+					return min + ((xx + 3) * (1.0 / 6.0)) * range;
+				case 2: // PART_EDISTR_INVGAUSSIAN
+					do
+					{
+						xx = (GMRandom.fYYRandom() - 0.5) * 6;
+					} while (!(Math.Exp(-(xx * xx) * 0.5) > GMRandom.fYYRandom()));
+
+					if (xx < 0)
+					{
+						xx += 6;
+					}
+
+					return min + (xx * (1.0 / 6.0)) * range;
+				default:
+					throw new ArgumentOutOfRangeException();
+			}
 		}
 	}
 }
