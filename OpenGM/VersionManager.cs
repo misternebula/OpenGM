b4fcf288eb381453c6d1a10038a8c698988d9435
JohnCorby <williamforgames6@gmail.com>
2025-07-05 14:57:19 -0700

    oops i meant to remove this as well

--- OpenGM/VersionManager.cs
+++ OpenGM/VersionManager.cs
@@ -33,9 +33,6 @@ namespace OpenGM
             Build = versionParts[3];
         }
 
-        public readonly bool IsGMS1() => Major == 1;
-        public readonly bool IsGMS2() => Major >= 2;
-
         public override readonly string ToString()
         {
             return $"{Major}.{Minor}.{Release}.{Build}";
