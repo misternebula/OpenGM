11a796053d486d40e3fa1a4559ed209c1f9ae5c8
_nebula <41904486+misternebula@users.noreply.github.com>
2025-07-25 10:17:33 +0100

    add logs to roomcc envstack weirdness

--- OpenGM/RoomManager.cs
+++ OpenGM/RoomManager.cs
@@ -547,8 +547,31 @@ public static class RoomManager
             VMExecutor.ExecuteCode(createCode, dummy);
             dummy = null;
 
-            // HACK: why does the dummy object stay on envstack? no idea!!!
-            VMExecutor.EnvStack.Clear();
+            if (VMExecutor.EnvStack.Count != 0)
+            {
+                DebugLog.LogWarning("EnvStack not empty after room creation code!");
+                foreach (var item in VMExecutor.EnvStack)
+                {
+                    if (item == null)
+                    {
+                        DebugLog.LogWarning(" - NULL");
+                    }
+                    else
+                    {
+                        if (item.Self is GamemakerObject)
+                        {
+                            DebugLog.LogWarning($" - {item.ObjectDefinition?.Name} ({item.GMSelf.instanceId})");
+                        }
+                        else
+                        {
+                            DebugLog.LogWarning($" - {item.GetType()}");
+                        }
+                    }
+                }
+
+                // HACK: why does the dummy object stay on envstack? no idea!!!
+                VMExecutor.EnvStack.Clear();
+            }
         }
 
         DebugLog.LogInfo($"Calling RoomStart...");
