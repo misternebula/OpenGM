515bfa1db98e25bef247f49b6376bf62ff2e90fd
TaiAurori <taiku@devkats.club>
2025-07-27 20:22:48 -0400

    don't run *anything* on inactive objects

--- OpenGM/Rendering/DrawManager.cs
+++ OpenGM/Rendering/DrawManager.cs
@@ -163,7 +163,7 @@ public static class DrawManager
         // g_pLayerManager.UpdateLayers();
         // g_pSequenceManager.PerformInstanceEvents(g_RunRoom, EVENT_STEP_BEGIN);
 
-        var stepList = _drawObjects.OrderBy(x => x.instanceId);
+        var stepList = _drawObjects.Where(x => x is not GamemakerObject obj || obj.Active).OrderBy(x => x.instanceId);
         if (RunStepScript(stepList, EventSubtypeStep.BeginStep))
         {
             return;
@@ -291,7 +291,7 @@ public static class DrawManager
     {
         foreach (var item in stepList)
         {
-            if (item is GamemakerObject gm && gm.Active)
+            if (item is GamemakerObject gm)
             {
                 gm.UpdateAlarms();
             }
